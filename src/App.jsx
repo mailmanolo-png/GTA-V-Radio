ef7cb9bf4502eb9c55be0b5d8a460ce5b4acfe20
Manolo Besseling <besseling@Manolos-Mini.home>
2026-06-23 14:30:03 +0200

    Add app splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { RadioWheel } from "./RadioWheel"
+import { useEffect, useState } from "react";
+import { RadioWheel } from "./RadioWheel";
 
 function App() {
-  	return ( 
-		<div className='app'>
-			<RadioWheel></RadioWheel>
-		</div>
-  	)
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="app">
+      {showSplash && (
+        <div className="splash-screen">
+          <img src="/icons/icon-512.png" alt="GTA Radio" />
+          <h1>GTA Radio</h1>
+          <p>Loading stations...</p>
+        </div>
+      )}
+
+      <RadioWheel />
+    </div>
+  );
 }
 
-export default App
+export default App;
